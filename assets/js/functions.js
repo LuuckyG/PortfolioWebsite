54d4a4364b1a97ed16652f62c97e57e382cffb0c
Luuk <luuk.geelen@hotmail.com>
2020-06-12 17:52:38 +0200

    Added sticky navbar

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,20 @@
-$( document ).ready(function() {
 
-  // Get started!
+$(document).ready(function(){
 
+});
+
+$(function() {
+  window.addEventListener(
+    'scroll',
+    function() {
+      var navbar = document.getElementById("top-page");
+      var sticky = navbar.offsetTop;
+
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+
+    });
 });
