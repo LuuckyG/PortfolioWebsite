27ef9547c0394f0396e1706b6b4b634385b71354
Luuk <luuk.geelen@hotmail.com>
2020-12-17 19:09:36 +0100

    First commit react version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import PersonalWebsite from "./PersonalWebsite/site";
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalWebsite></PersonalWebsite>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PersonalWebsite/site.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import pageHeader from './pages/header';
+import pageFooter from './pages/footer';
+
+import './PathfindingVisualizer.css';
+
+
+export default class PersonalWebsite extends Component {
+    constructor() {
+        super();
+        this.state = {
+            webContent = [],
+        };
+    }
+}
